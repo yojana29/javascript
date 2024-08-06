625d6a8670c2fa26415a3231b549598fce893df8
Shekhar969 <shekharrawal96@gmail.com>
2024-08-06 20:36:42 +0545

    initial commit By Shekhar

---
+++ shekhar/script.js
@@ -0,0 +1,9 @@
+let name=prompt("Enter the name ");
+let gen=prompt("Enter M for male and F for faltu ")
+let age=prompt("Enter the age");
+
+if (age>19 && gen=="M"){
+    console.log("You can ")
+}else{
+    console.log("who are you vai?")
+};
